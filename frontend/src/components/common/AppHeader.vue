1635797eb9bcb86992480f2261311854c98dbb2b
LEEYOSEPH <laksmi0628@gmail.com>
2021-08-02 20:18:17 +0900

    feat: AppHeader 컴포넌트 생성
    components 폴더에 common 폴더 만들었고 AppHeader.vue 파일 생성 했습니다.
    로고 로그인 회원가입 링크 만들었고, 각각 라우터링크로 연결 했습니다.

---
+++ frontend/src/components/common/AppHeader.vue
@@ -0,0 +1,61 @@
+<template>
+  <header>
+    <div>
+      <router-link to="/" class="logo">GE-SI-PAN</router-link>
+    </div>
+    <div class="navigations">
+      <router-link to="/login">로그인</router-link>
+      <router-link to="/signup">회원가입</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.username {
+  color: white;
+}
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: #707070;
+  z-index: 2;
+  box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.05);
+}
+a {
+  color: #dedede;
+  font-size: 18px;
+  text-decoration: none;
+}
+a.logo {
+  font-size: 30px;
+  font-weight: 900;
+  color: white;
+}
+.logo > span {
+  font-size: 14px;
+  font-weight: normal;
+}
+.navigations a {
+  margin-left: 10px;
+}
+.fixed {
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+.logout-button {
+  font-size: 14px;
+}
+a.router-link-exact-active {
+  color: white;
+  font-weight: bold;
+}
+</style>
