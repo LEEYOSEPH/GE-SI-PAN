05b915d99cd27b5b721f02b9567a2f929da583f1
YOSEPHLEE <laksmi0628@gmail.com>
2021-08-03 09:52:40 +0900

    feat: 메인페이지 컴포넌트 생성

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import LoginPage from "@/views/LoginPage.vue";
 import SignupPage from "@/views/SignupPage.vue";
 import MainPage from "@/views/MainPage.vue";
+import AddPost from "@/views/AddPost.vue";
 
 Vue.use(VueRouter);
 
@@ -14,16 +15,23 @@ const router = new VueRouter({
       redirect: "/main",
     },
     {
+      /* 메인페이지 */
       path: "/main",
       component: MainPage,
     },
     {
+      /* 로그인페이지 */
       path: "/login",
       component: LoginPage,
     },
     {
+      /* 회원가입페이지 */
       path: "/signup",
       component: SignupPage,
+    },
+    {
+      path: "/add",
+      component: AddPost,
     },
   ],
 });
--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,9 +1,37 @@
 <template>
-  <div>메인</div>
+  <div>
+    <div class="main list-container contents">
+      <h1 class="page-header">GE SI PAN</h1>
+      <loading-spinner v-if="isloding"></loading-spinner>
+      <ul v-else>
+        <div>게시글</div>
+      </ul>
+    </div>
+    <router-link to="/add" class="create-button">
+      <i class="ion-md-add"></i>
+    </router-link>
+  </div>
 </template>
 
 <script>
-export default {};
+import LoadingSpinner from "../components/common/LoadingSpinner.vue";
+export default {
+  components: { LoadingSpinner },
+  data() {
+    return {
+      isLoding: false,
+    };
+  },
+  methods: {
+    fetchData() {
+      this.isLoding = true;
+      this.isLoding = false;
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+};
 </script>
 
 <style></style>
